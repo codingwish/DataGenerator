4f646c05c7e89a1c53e9d50d6214149c8ef5c44c
Daniel Wunsch <d.wunsch@sectorsoft.de>
2021-10-07 15:36:59 +0200

    Projektdateien hinzufügen.

---
+++ DataGenerator.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataGenerator.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
